baf67bf2c1a0eb61dfe248d9d0f9c4a9f1d3f2c0
ragulkarthick832 <ragulkarthick832@gmail.com>
2025-01-08 23:56:05 +0530

    cleaner-version-of-async-read-of-two-files

--- 13-async-eventLoop.js
+++ 13-async-eventLoop.js
@@ -1,24 +1,29 @@
-const http = require('http')
+const {readFile} = require('fs');
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end('Home Page')
-    }
-    else if (req.url === '/about'){
-        // BLOCKING CODE !!!
-        for(let i = 0; i < 1000; i++){
-            for(let j = 0; j < 1000; j++){
-                console.log(`${i} ${j}`)
+const getText = (path) => {
+    return new Promise ((resolve,reject)=>{
+        readFile(path,'utf-8',(err,data)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
             }
-        }
-        res.end('About Page')
-    }
-    else{
-    res.end('Error Page')
-    }
-})
+        })
+    })
+}
 
+// getText('../content/first.txt')
+//     .then(result => console.log(result))
+//     .catch((err) => console.log(err))
 
-server.listen(5000,()=>{
-    console.log('Server Listening on port 5000')
-})
+const start = async() => {
+    try {
+        const first = await getText('./content/first.txt')
+        const second = await getText('./content/second.txt')
+
+        console.log(first,second)
+    } catch (error) {
+        console.log(error)   
+    }
+}
+start()
