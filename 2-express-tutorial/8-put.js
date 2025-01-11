41456a73f89264e635b45db3a503d8eaf436fb04
ragulkarthick832 <ragulkarthick832@gmail.com>
2025-01-11 22:41:28 +0530

    put-method

---
+++ 2-express-tutorial/8-put.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+let {people} = require('./data')
+
+
+// static assests
+
+app.use(express.static('./methods-public'))
+// parse from the data
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.json());
+
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
+})
+
+
+app.post('/login',(req,res)=>{
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide Credentials')
+})
+
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'please provide name value'})
+    }
+    res.status(201).json({success:true,person:name})
+})
+
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person) => person.id === Number(id))
+
+
+    if (!person){
+        return res
+            .status(404)
+            .json({success:false,msg:`no person with id ${id}`})
+    }
+    const newPeople = people.map((person)=>{
+        if (person.id == Number(id)){
+            person.name = name
+        }
+        return person
+    })
+
+    res.status(200).json({success: true, data: newPeople})
+
+})
+
+app.listen(5000,()=>{
+    console.log('Server is listening on port 5000...')
+})
