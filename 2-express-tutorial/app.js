50fb6151890374e842da12265c91d081414e250c
ragulkarthick832 <ragulkarthick832@gmail.com>
2025-01-11 20:06:25 +0530

    minor update

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,58 +1,30 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+// express will supply the required flow logic  -- next paramter 
+
+app.use('/api',logger)
+
+app.use('/',authorize)
+
+app.get('/',(req,res)=>{
+    
+    res.send('Home')
 })
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
-
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
-
-  return res.json(singleProduct)
+app.get('/about',(req,res)=>{
+    res.send('About')
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
 
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
+app.get('/api/about',(req,res)=>{
+    res.send('Products')
 })
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.get('/api/items',(req,res)=>{
+    res.send('Ites')
 })
+app.listen(5000,()=>{
+    console.log('Server is listening on port 5000')
+}) 
