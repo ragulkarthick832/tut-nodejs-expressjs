f3ab12555a2363cea8c6fbad0fab0dca00da2993
ragulkarthick832 <ragulkarthick832@gmail.com>
2025-01-12 15:19:56 +0530

    added controllers and route setup

--- 3-task-manager/starter/app.js
+++ 3-task-manager/starter/app.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const app = express();
+const tasks = require('./routes/tasks')
+
+app.use(express.json())
 
 //routes
-app.get('/',(req,res)=>{
+app.use('/api/v1/tasks',tasks)
+
+app.get('/hello',(req,res)=>{
     res.send('Task Manager App')
 })
 
-
-// app.get('/api/v1/tasks) - get all the task
-// app.post('/api/v1/tasks) - create a new task
-// app.get('/api/v1/tasks/:id) - get single task
-// app.patch('/api/v1/tasks/:id') - update task
-// app.delete('/api/v1/tasks/:id)  - delete task
+// app.get('/api/v1/tasks)          - get all the task
+// app.post('/api/v1/tasks)         - create a new task
+// app.get('/api/v1/tasks/:id)      - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id)   - delete task
 
 const port = 3000
 
---
+++ 3-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString = 
+ 'mongodb+srv://<db_username>:<db_password>@nodejstutorial.ffkg9.mongodb.net/?retryWrites=true&w=majority&appName=NodejsTutorial'
