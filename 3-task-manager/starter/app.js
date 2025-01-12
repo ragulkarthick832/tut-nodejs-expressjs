8b01fc9214b1cf8e6a579e7dee92fadc06e54ca7
ragulkarthick832 <ragulkarthick832@gmail.com>
2025-01-12 08:22:23 +0530

    setup

---
+++ 3-task-manager/starter/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+
+//routes
+app.get('/hello',(req,res)=>{
+    res.send('Task Manager App')
+})
+
+
+// app.get('/api/v1/tasks) - get all the task
+// app.post('/api/v1/tasks) - create a new task
+// app.get('/api/v1/tasks/:id) - get single task
+// app.patch('/api/v1/tasks/:id') - update task
+// app.delete('/api/v1/tasks/:id)  - delete task
+
+const port = 3000
+
+app.listen(port,console.log(`server is listening on ${port}....`))
